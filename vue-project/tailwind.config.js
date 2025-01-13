543ddded58cffb17b3cc2b0a9ed6a0735fcaafeb
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-01-13 14:17:23 -0700

    feature/VWA-01: Creating site navigation, adjusting tailwind config

--- vue-project/tailwind.config.js
+++ vue-project/tailwind.config.js
@@ -1,25 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', 'src/**/*.{vue,js,ts,jsx,tsx'],
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         'weather-primary': '#00668A',
-        'weather-secondary': '#004E71'
+        'weather-secondary': '#004E71',
       },
     },
     fontFamily: {
-      Roboto: ['Roboto, sans-serif']
+      Roboto: ['Roboto, sans-serif'],
     },
     container: {
       padding: '2rem',
-      center: true
+      center: true,
     },
     screens: {
       sm: '640px',
-      md: '768px'
+      md: '768px',
     },
   },
   plugins: [],
-}
-
+};
